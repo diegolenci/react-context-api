b49a14299a1a4dc2bd477f5ee94f0612a617fdf2
diegolenci <lencidiego0@gmail.com>
2025-03-04 17:03:29 +0100

    import GlobalProvider in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import HomePage from "./pages/HomePage"
 import ContactPage from "./pages/ContactPages"
 import PostList from "./pages/PostList"
 import SinglePost from "./pages/SinglePost"
+//importazione context
+import { GlobalProvider } from "./context/GlobalContext"
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={HomePage}/>
-        <Route path="/chiSiamo" Component={ContactPage}/>
-        <Route path="/list" Component={PostList}/>
-        <Route path="/list/:id" Component={SinglePost}/>
-      </Routes>
-      </BrowserRouter>
+      <GlobalProvider>
+        <BrowserRouter>
+         <Routes>
+           <Route path="/" Component={HomePage}/>
+           <Route path="/chiSiamo" Component={ContactPage}/>
+           <Route path="/list" Component={PostList}/>
+           <Route path="/list/:id" Component={SinglePost}/>
+         </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
     </>
   )
 }
